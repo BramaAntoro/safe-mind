29c05d187cf3cae36e33ee54dbbdbf6a7238616d
BramaAntoro <brama4751antoro@gmail.com>
2026-05-19 11:19:14 +0700

    add feat auth: login, register, logout with supabase UI

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,6 +1,10 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+/**
+ * If using Fluid compute: Don't put this client in a global variable. Always create a new client within each
+ * function when using it.
+ */
 export async function createClient() {
   const cookieStore = await cookies()
 
@@ -12,7 +16,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet, _headers) {
+        setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
